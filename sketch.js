6fb1452c69ee28a152e1b9d3f0e3d8bd93348b71
montoyamoraga <montoyamoraga@gmail.com>
2019-02-14 02:29:58 -0500

    add first draft

---
+++ sketch.js
@@ -0,0 +1,51 @@
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Style Transfer Image Example using p5.js
+This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young American Girl, The Dance)
+=== */
+
+let inputImg;
+let statusMsg;
+let transferBtn;
+let style1;
+let style2;
+
+function setup() {
+  noCanvas();
+  // Status Msg
+  statusMsg = select('#statusMsg');
+
+  // Get the input image
+  inputImg = select('#inputImg');
+
+  // Transfer Button
+  transferBtn = select('#transferBtn');
+  transferBtn.mousePressed(transferImages);
+
+  // Create style method with different pre-trained models
+  style = ml5.styleTransfer('models/tmnt', modelLoaded);
+}
+
+// A function to be called when the models have loaded
+function modelLoaded() {
+  // Check if both models are loaded
+  if(style.ready){
+    statusMsg.html('Ready!')
+  }
+}
+
+// Apply the transfer to both images!
+function transferImages() {
+  statusMsg.html('Applying Style Transfer...!');
+
+  style.transfer(inputImg, function(err, result) {
+    createImg(result.src).parent('style');
+  });
+
+  statusMsg.html('Done!');
+}
